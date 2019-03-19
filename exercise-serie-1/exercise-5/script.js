6339f6a0e493b1348b2595b9e61a42718e2a1409
luca <lucavhw@gmail.com>
2019-03-19 09:44:18 +0100

    updated

--- exercise-serie-1/exercise-5/script.js
+++ exercise-serie-1/exercise-5/script.js
@@ -3,3 +3,17 @@ console.log("This script is linked properly!")
 
 
 //Write your JS code here...
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}
+  x[myIndex-1].style.display = "block";
+  setTimeout(carousel, 1000); // Change image every 2 seconds
+}
